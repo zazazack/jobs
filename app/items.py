aa4eb49371e50b201d77f61c7994e2b841ac7224
Zachary Wilson <wilsonze@gmail.com>
2018-07-19 20:10:25 -0500

    Cleanup and feature improvements

--- app/items.py
+++ app/items.py
@@ -8,22 +8,21 @@
 import re
 from datetime import datetime as dt
 
-# import dateparser
+import dateparser
+from w3lib.html import remove_tags, strip_html5_whitespace
 import scrapy
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import MapCompose, TakeFirst
 
 
-def build_job_post_url_with_jobkey(jobkey):
+def build_post_url(jobkey):
     return f'https://www.indeed.com/viewjob?jk={jobkey}'
 
 
-def is_sponsored(s):
-    s = s.lower().strip()
-    if 'sponsored' in s:
-        return True
-    else:
-        return False
+def post_age_to_datetime(post_age: str) -> str:
+    if isinstance(post_age, str):
+        dt = dateparser.parse(post_age.replace('+', ''))
+        return dt.isoformat(timespec='seconds', sep=' ')
 
 
 class JobLoader(ItemLoader):
@@ -32,18 +31,21 @@ class JobLoader(ItemLoader):
 
 
 class Job(scrapy.Item):
-    company = scrapy.Field()
     id = scrapy.Field()
-    job_type = scrapy.Field() # full-time, part-time, etc.
-    jobtitle = scrapy.Field()
-    location = scrapy.Field()
     post_age = scrapy.Field()
-    post_url = scrapy.Field(input_processor=MapCompose(build_job_post_url_with_jobkey))
-    sponsored = scrapy.Field(input_processor=MapCompose(is_sponsored))
-    summary = scrapy.Field()
-
-class Page(scrapy.Item):
-    id = scrapy.Field()
-    page_text = scrapy.Field()
-    page_title = scrapy.Field()
-    url = scrapy.Field()
+    post_dt = scrapy.Field(input_processor=MapCompose(post_age_to_datetime))
+    post_url = scrapy.Field(input_processor=MapCompose(build_post_url))
+    company = scrapy.Field(input_processor=MapCompose(remove_tags, strip_html5_whitespace))
+    company_score = scrapy.Field()
+    job_category = scrapy.Field()
+    job_industry = scrapy.Field()
+    job_requirements = scrapy.Field()
+    job_score = scrapy.Field()
+    job_summary = scrapy.Field(input_processor=MapCompose(remove_tags, strip_html5_whitespace))
+    job_title = scrapy.Field(input_processor=MapCompose(remove_tags, strip_html5_whitespace))
+    job_type = scrapy.Field()  # full-time, part-time, etc.
+    salary_estimate = scrapy.Field()
+    location = scrapy.Field()
+    city = scrapy.Field()
+    state = scrapy.Field()
+    zip_code = scrapy.Field()
